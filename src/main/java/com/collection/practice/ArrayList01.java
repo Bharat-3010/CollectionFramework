4b569d5ac77675724cdef9039abc131cb2370dfa
Bharat <bharatmohite042@gmail.com>
2026-01-11 17:25:05 +0530

    collection Start

---
+++ src/main/java/com/collection/practice/ArrayList01.java
@@ -0,0 +1,28 @@
+package com.collection.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList01 {
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+		
+		list.add("Amol");
+		list.add("Dipak");
+		list.add("Sahil");
+		list.add("Dipak");
+		list.add("Shivraj");
+		
+		System.out.println(list);
+		System.out.println(list.size());
+		
+//		try {
+		System.out.println(list.get(5));  // IndexOutOfBoundException
+//		}catch (Exception e) {
+//			e.printStackTrace();
+//		}
+		System.out.println(list.get(4));
+	}
+
+}
