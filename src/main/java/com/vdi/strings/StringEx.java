22e390d44aa50371c1e951cb8c9be3d281824806
Bharat-3010 <bharatmohite042@gmail.com>
2026-01-14 14:09:25 +0530

    Clarify string creation with comments
    Added comments to clarify string creation and comparison.

--- src/main/java/com/vdi/strings/StringEx.java
+++ src/main/java/com/vdi/strings/StringEx.java
@@ -3,8 +3,8 @@ package com.vdi.strings;
 public class StringEx {
 	
 	public static void main(String[] args) {
-		String s1="FullStack";
-		String s2= new String("FullStack");
+		String s1="FullStack";                // String Address stored in SCP
+		String s2= new String("FullStack");   // Object Stored in heap area
 	
 		System.out.println(s1==s2);
 		System.out.println(s1.equals(s2));
