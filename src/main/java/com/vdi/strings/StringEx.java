4e3f7271b216f9dbd62a7f616075213010535ada
Bharat <bharatmohite042@gmail.com>
2026-01-14 14:04:20 +0530

    Strings In Java

---
+++ src/main/java/com/vdi/strings/StringEx.java
@@ -0,0 +1,13 @@
+package com.vdi.strings;
+
+public class StringEx {
+	
+	public static void main(String[] args) {
+		String s1="FullStack";
+		String s2= new String("FullStack");
+	
+		System.out.println(s1==s2);
+		System.out.println(s1.equals(s2));
+	}
+
+}
